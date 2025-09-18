033ce05bf04f89702276210d529be90548f8f561
Vipin Dev <anzirish@gmail.com>
2025-09-18 08:46:45 +0530

    create navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
+import { Link, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Trips from "./pages/Trips";
 
 function App() {
   return (
     <>
-      <p>Hello</p>
+      <div className="min-h-screen">
+        <header>
+          <div className="bg-white shadow-sm p-4">
+            <div className="container mx-auto flex items-center justify-between">
+              <Link to="/trips" className="text-2xl font-semibold">
+                Travel Planner
+              </Link>
+            </div>
+          </div>
+        </header>
+
+        <main className="container mx-auto p-4">
+          <Routes>
+            <Route path="/" element={<Trips />} />
+            <Route path="/trips" element={<Trips />} />
+            <Route path="/trip/:id" element={<Trips />} />
+          </Routes>
+        </main>
+      </div>
     </>
   );
 }
