7178997e6dd5810ba210ac5bf0e0e8e1d93d477a
Vipin Dev <anzirish@gmail.com>
2025-09-20 22:43:00 +0530

    remove default itineray form

--- src/components/trip/ItineraryTab.tsx
+++ src/components/trip/ItineraryTab.tsx
@@ -1,18 +1,10 @@
 import DayView from "../../features/itinarery/DayView";
-import ItemEditor from "../../features/itinarery/ItemEditor";
 import type { Trip } from "../../Types/trip";
 
 export default function ItineraryTab({ trip }: { trip: Trip }) {
   return (
     <div>
       <DayView trip={trip} />
-      <div className="mt-4">
-        <ItemEditor
-          date={trip.startDate}
-          tripId={trip.id}
-          onClose={() => {}}
-        />
-      </div>
     </div>
   );
 }
